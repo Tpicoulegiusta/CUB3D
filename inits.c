b2bb2dc327f3b89b3ea43a5a3ed42e00c1b1fd1c
Tommi Picoulet-Giusta <tpicoule@c1r3p1.42nice.fr>
2024-04-29 15:23:36 +0200

    ooooh

--- inits.c
+++ inits.c
@@ -6,21 +6,55 @@
 /*   By: tpicoule <tpicoule@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 16:46:37 by tpicoule          #+#    #+#             */
-/*   Updated: 2024/04/23 16:42:13 by tpicoule         ###   ########.fr       */
+/*   Updated: 2024/04/29 15:22:38 by tpicoule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	camera_dir_set(t_game *game)
+{
+	if (game->player == 'E')
+	{
+		game->data.dir_x = 0;
+		game->data.dir_y = 1;
+		game->data.plane_x = 0.66;
+		game->data.plane_y = 0;
+	}
+	if (game->player == 'W')
+	{
+		game->data.dir_x = 0;
+		game->data.dir_y = -1;
+		game->data.plane_x = -0.66;
+		game->data.plane_y = 0;
+	}
+}
+
+void	camera_dir_set2(t_game *game)
+{
+	if (game->player == 'N')
+	{
+		game->data.dir_x = -1;
+		game->data.dir_y = 0;
+		game->data.plane_x = 0;
+		game->data.plane_y = 0.66;
+	}
+	if (game->player == 'S')
+	{
+		game->data.dir_x = 1;
+		game->data.dir_y = 0;
+		game->data.plane_x = 0;
+		game->data.plane_y = -0.66;
+	}
+}
 
 void	data_init(t_game *game)
 {
 	game->data.mlx = mlx_init();
 	game->data.pos_x = game->y_player + 0.5;
 	game->data.pos_y = game->x_player + 0.5;
-	game->data.dir_x = -1;
-	game->data.dir_y = 0;
-	game->data.plane_x = 0;
-	game->data.plane_y = 0.66;
+	camera_dir_set(game);
+	camera_dir_set2(game);
 	game->data.up = 0;
 	game->data.down = 0;
 	game->data.left = 0;
